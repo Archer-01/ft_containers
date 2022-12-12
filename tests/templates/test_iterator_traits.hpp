dfa7d5585001c48d489bcbea929059529957dce9
Hamza Haddani <hhamza@student.1337.ma>
2022-12-12 10:12:50 +0100

    style: Re-format iterator traits tests

--- tests/templates/test_iterator_traits.hpp
+++ tests/templates/test_iterator_traits.hpp
@@ -1,24 +1,25 @@
 #pragma once
 
-#include <vector>
-
 #include "iterator.hpp"
 #include "tests.hpp"
+#include <vector>
 
-template <template<typename Iterator> class IteratorTraits>
-void	iterator_traits_vector()
+template <template <typename Iterator> class IteratorTraits>
+void iterator_traits_vector()
 {
 	typedef IteratorTraits<std::vector<int>::iterator> traits;
 
-	assert(typeid(typename traits::difference_type) == typeid(std::vector<int>::iterator::difference_type));
+	assert(typeid(typename traits::difference_type) ==
+		   typeid(std::vector<int>::iterator::difference_type));
 	assert(typeid(typename traits::value_type) == typeid(int));
 	assert(typeid(typename traits::pointer) == typeid(int *));
 	assert(typeid(typename traits::reference) == typeid(int &));
-	assert(typeid(typename traits::iterator_category) == typeid(std::random_access_iterator_tag));
+	assert(typeid(typename traits::iterator_category) ==
+		   typeid(std::random_access_iterator_tag));
 }
 
-template <template<typename Iterator> class IteratorTraits>
-void	iterator_traits_int_ptr()
+template <template <typename Iterator> class IteratorTraits>
+void iterator_traits_int_ptr()
 {
 	typedef IteratorTraits<int *> traits;
 
@@ -26,10 +27,11 @@ void	iterator_traits_int_ptr()
 	assert(typeid(typename traits::value_type) == typeid(int));
 	assert(typeid(typename traits::pointer) == typeid(int *));
 	assert(typeid(typename traits::reference) == typeid(int &));
-	assert(typeid(typename traits::iterator_category) == typeid(std::random_access_iterator_tag));
+	assert(typeid(typename traits::iterator_category) ==
+		   typeid(std::random_access_iterator_tag));
 }
 
-template <template<typename Iterator> class IteratorTraits>
+template <template <typename Iterator> class IteratorTraits>
 void iterator_traits_const_int_ptr()
 {
 	typedef IteratorTraits<const int *> traits;
@@ -38,5 +40,6 @@ void iterator_traits_const_int_ptr()
 	assert(typeid(typename traits::value_type) == typeid(int));
 	assert(typeid(typename traits::pointer) == typeid(const int *));
 	assert(typeid(typename traits::reference) == typeid(const int &));
-	assert(typeid(typename traits::iterator_category) == typeid(std::random_access_iterator_tag));
+	assert(typeid(typename traits::iterator_category) ==
+		   typeid(std::random_access_iterator_tag));
 }
